eb8fe2402343169b97145cf990970373bd22eca9
Chris Gong <christophergong@berkeley.edu>
2018-06-28 15:12:44 -0700

    beginning for the day

--- lib/Restaurant.dart
+++ lib/Restaurant.dart
@@ -7,8 +7,10 @@ class Restaurant {
   Restaurant.fromJson(Map<String, dynamic> json) {
     this.name = json['name'];
     this.lat =  json['geometry']['location']['lat'];
-    this.lon = json['lon'];
-
-
+    this.lon = json['geometry']['location']['lng'];
+    this.rating = json['rating'];
+    this.address = json['vicinity'];
+    this.id = json['id'];
+    this.icon = json['icon'];
   }
 }
